99a9d4f242bc4e45074012af6f0406d6ac47fb8a
jeric <rinoa0424@gmail.com>
2019-05-16 20:54:53 +0800

    Adding line chart

--- app/Http/Controllers/Dashboard/Dashboardcontroller.php
+++ app/Http/Controllers/Dashboard/Dashboardcontroller.php
@@ -17,51 +17,6 @@ class DashboardController extends Controller
     return view( 'dashboard',['account'=>$account,'users'=>$users]);
   }
 
-  public function casescount(Request $request)
-  {
-	$r1 = explode("-", $request->range);
-    $date=date_create($r1[0]);
-    $from = date_format($date,"Y-m-d H:i:s");
-    $date=date_create($r1[1]);
-    $to = date_format($date,"Y-m-d H:i:s");
-
-  	if($request->account_id == "all")
-  	{
-		 $cases = Cases::leftJoin('accounts as b','b.id','=','cases.account_id')->whereBetween('cases.created_at',[$from,$to])
-  					->select(DB::raw('count(0) as value'),'b.account_name as name')->groupBy('cases.account_id')
-  					->calltype($request->call_type)
-  					->subcalltype($request->subcall_type)
-  					->get();
-
-  		  	return json_encode($cases);
-  	}if($request->call_type == "all")
-    {
-      $cases = Cases::whereBetween('cases.created_at',[$from,$to])
-            ->account($request->account_id)
-            ->select('cases.call_type as name',DB::raw('count(0) as value'))->groupBy('cases.call_type')
-            ->calltype($request->call_type)
-            ->subcalltype($request->subcall_type)
-            ->get();
-
-          return json_encode($cases);
-    }else
-  	{
-  		$cases = Cases::whereBetween('cases.created_at',[$from,$to])
-  					->account($request->account_id)
-  					->select('cases.subcall_type as name',DB::raw('count(0) as value'))->groupBy('cases.subcall_type')
-  					->calltype($request->call_type)
-  					->subcalltype($request->subcall_type)
-  					->get();
-
-  		return json_encode($cases);
-  	}
-
-
- 
-
-
-  }
-
   public function checkuser(Request $request)
   {
 
