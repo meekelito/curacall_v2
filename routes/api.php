0046156bd6d42431722d0815ae26669c27b30b6f
Kael <artmichael08@gmail.com>
2019-05-17 14:42:19 +0800

    commit update
    commit update

--- routes/api.php
+++ routes/api.php
@@ -12,31 +12,46 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'Api\AuthController@login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'middleware' => 'jwt.auth',
+], function ($router) {
+	Route::get('/cases/{status?}/{user_id}', 'Api\ApiController@getCases' ); 
+
+	Route::get('/case-preview', 'Api\ApiController@getCaseSpecific' ); 
+
+	Route::get('/case-participants', 'Api\ApiController@getParticipants' ); 
+
+	Route::get('/case-count', 'Api\ApiController@getCaseCount' ); 
+
+	Route::post('/case-accept', 'Api\ApiController@acceptCase' );  
+
+	Route::post('/case-close', 'Api\ApiController@closeCase' ); 
+
+	Route::post('/case-new', 'Api\ApiController@newCase' );  
+
+	Route::post('/case-test', 'Api\ApiController@testCase' ); 
+
+});
+
+// Route::fallback(function(){
+//     return response()->json([
+//         'message' => 'Page Not Found. If error persists, contact info@curacall.com'], 404);
+// });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::get('/cases/{status?}/{user_id}', 'Api\ApiController@getCases' ); 
 
-Route::get('/case-preview', 'Api\ApiController@getCaseSpecific' ); 
 
-Route::get('/case-participants', 'Api\ApiController@getParticipants' ); 
 
-Route::get('/case-count', 'Api\ApiController@getCaseCount' ); 
 
-Route::post('/case-accept', 'Api\ApiController@acceptCase' );  
-
-Route::post('/case-close', 'Api\ApiController@closeCase' ); 
-
-Route::post('/case-new', 'Api\ApiController@newCase' );  
-
-Route::post('/case-test', 'Api\ApiController@testCase' );  
-
-Route::fallback(function(){
-    return response()->json([
-        'message' => 'Page Not Found. If error persists, contact info@curacall.com'], 404);
-});
 
 
 
