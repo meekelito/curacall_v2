8db8ae1e8509e2e4da5619c4c3b1c6f180017846
jeric <rinoa0424@gmail.com>
2019-05-25 22:09:42 +0800

    assign permission to roles

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -15,15 +15,341 @@ class RolePermissionSeeder extends Seeder
     {
     	// curacall admin role's permission
         $role = Role::findByName('curacall-admin');
-		$role->givePermissionTo(Permission::all());
+		  $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting',               
+                    // Generate Information
+                    'edit-company-information',         
+                    'edit-support-contact-information',  
+                    //Case Management
+                    'manage-case',                      
+                    //Billing
+                    'view-billing-report',              
+                    //Curacall Admin Console
+                    'manage-general-info',              
+                    'manage-roles',                      
+                    'manage-users',                     
+                    'manage-accounts',                  
+                    'manage-billing'
+               ]
+            );
 
 		// curacall maangement role's permission
         $role = Role::findByName('curacall-management');
-		$role->givePermissionTo(Permission::all());
+		$role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting',               
+                    // Generate Information
+                    'edit-company-information',         
+                    'edit-support-contact-information',  
+                    //Case Management
+                    'manage-case',                      
+                    //Billing
+                    'view-billing-report',              
+               ]
+            );
 
 		// curacall maangement role's permission
         $role = Role::findByName('curacall-user');
-		$role->givePermissionTo(Permission::all());
+		  $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting'
+               ]
+            );
+
+        // curacall maangement role's permission
+        $role = Role::findByName('account-admin');
+        $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting',               
+                    // Generate Information
+                    'edit-company-information',         
+                    'edit-support-contact-information',  
+                    //Case Management
+                    'manage-case',                      
+                    //Billing
+                    'view-billing-report',              
+               ]
+            );
+
+        // curacall maangement role's permission
+        $role = Role::findByName('agency-management');
+        $role->givePermissionTo(
+           [
+                //Dashboard
+                'view-account-reports',
+                'view-oncall-reports',
+                // Cases
+                'view-all-cases',                  
+                'view-active-cases', 
+                'view-pending-cases',
+                'view-closed-cases', 
+                'view-silent-cases',
+                'forward-case',
+                'accept-case',
+                'close-case', 
+                'pull-case', 
+                'add-note',
+                'export-pdf',
+                'add-case-participant',  
+                 // Messages
+                'never-send-message-unless-message-received', 
+                'send-message-to-caregiver',            
+                'send-message-to-nursing',       
+                'send-message-to-coordinator',
+                'send-message-to-management',  
+                'send-message-to-account-admin',   
+                'send-message-to-anyone',  
+                // Contacts
+                'view-contacts',                
+                // Settings
+                'profile-setting',           
+                'security-login',                     
+                'message-setting',                    
+                'notification-setting',               
+                // Generate Information
+                'edit-company-information',         
+                'edit-support-contact-information',  
+                //Case Management
+                'manage-case',                      
+                //Billing
+                'view-billing-report',
+           ]
+        );
+
+        // curacall maangement role's permission
+        $role = Role::findByName('agency-nursing');
+         $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting'
+               ]
+            );
+
+        // curacall maangement role's permission
+        $role = Role::findByName('agency-coordinator');
+        $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting'
+               ]
+            );
+
+        // curacall maangement role's permission
+        $role = Role::findByName('agency-caregiver');
+        $role->givePermissionTo(
+               [
+                    //Dashboard
+                    'view-account-reports',
+                    'view-oncall-reports',
+                    // Cases
+                    'view-all-cases',                  
+                    'view-active-cases', 
+                    'view-pending-cases',
+                    'view-closed-cases', 
+                    'view-silent-cases',
+                    'forward-case',
+                    'accept-case',
+                    'close-case', 
+                    'pull-case', 
+                    'add-note',
+                    'export-pdf',
+                    'add-case-participant',  
+                     // Messages
+                    'never-send-message-unless-message-received', 
+                    'send-message-to-caregiver',            
+                    'send-message-to-nursing',       
+                    'send-message-to-coordinator',
+                    'send-message-to-management',  
+                    'send-message-to-account-admin',   
+                    'send-message-to-anyone',  
+                    // Contacts
+                    'view-contacts',                
+                    // Settings
+                    'profile-setting',           
+                    'security-login',                     
+                    'message-setting',                    
+                    'notification-setting'
+               ]
+            );
 
     }
 }
