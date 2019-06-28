b0edb20ca3c0cab2523b5a2e05051ffa5812463d
jeric <rinoa0424@gmail.com>
2019-06-28 20:55:12 +0800

    html5 browser notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 	<link href="{{ asset('assets/css/core.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/components.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/colors.css') }}" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="{{ asset('assets/js/plugins/notifications/notify.js') }}" ></script> 
+
 	<!-- /global stylesheets -->
 
 	<!-- Core JS files -->
@@ -512,6 +514,46 @@
          });
 
       }, 8000);
+
+        function onShowNotification () {
+            console.log('notification is shown!');
+        }
+        function onCloseNotification () {
+            console.log('notification is closed!');
+        }
+        function onClickNotification () {
+            console.log('notification was clicked!');
+        }
+        function onErrorNotification () {
+            console.error('Error showing notification. You may need to request permission.');
+        }
+        function onPermissionGranted () {
+            console.log('Permission has been granted by the user');
+            doNotification();
+        }
+        function onPermissionDenied () {
+            console.warn('Permission has been denied by the user');
+        }
+        function doNotification (title,body) {
+            var myNotification = new Notify(title, {
+                body: body,
+                tag: 'My unique id',
+                notifyShow: onShowNotification,
+                notifyClose: onCloseNotification,
+                notifyClick: onClickNotification,
+                notifyError: onErrorNotification,
+                timeout: 4
+            });
+            console.log('notify');
+            myNotification.show();
+
+        }
+        if (!Notify.needsPermission) {
+            doNotification();
+        } else if (Notify.isSupported()) {
+            Notify.requestPermission(onPermissionGranted, onPermissionDenied);
+        }
+
   </script>
 </html>
 
