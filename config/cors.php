771950a944aa644c20fd06b7d76f46294e8c1974
Robert Alan Gemong <snailbob@live.com>
2019-07-23 19:05:12 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -11,20 +11,13 @@ return [
     | to accept any value.
     |
     */
+   
     'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedOriginsPatterns' => [],
-    'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'X-Auth-Token',  'Access-Control-Allow-Origin'],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With', 'Access-Control-Allow-Origin'],
     'allowedMethods' => ['*'],
     'exposedHeaders' => [],
     'maxAge' => 0,
-    
-    // 'supportsCredentials' => true,
-    // 'allowedOrigins' => ['*'],
-    // 'allowedOriginsPatterns' => [],
-    // 'allowedHeaders' => ['*'], // Content-Type', 'X-Requested-With', 'X-Auth-Token', 'Authorization'],
-    // 'allowedMethods' => ['*'],
-    // 'exposedHeaders' => [],
-    // 'maxAge' => 0,
 
 ];
